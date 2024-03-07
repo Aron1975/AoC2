543b1cab7667ffb0ea926618c5b406566279fbe7
Aron Gyulai <arongyf@gmail.com>
2024-03-07 22:47:47 +0100

    Added comments

--- src/Day7_2023_Efter.kt
+++ src/Day7_2023_Efter.kt
@@ -1,2 +1,5 @@
 class Day7_2023_Efter {
+    //Har kollat på olika lösningar, de jag har hittat är snarlika min lösning.
+    //Eller också hade jag svårt att sätta mig in i dem. Companion Objects och Sequence är två
+    //av dem som jag ska försöka sätta mig in i.
 }
--- src/Day8_2023_Efter.kt
+++ src/Day8_2023_Efter.kt
@@ -1,2 +1,3 @@
 class Day8_2023_Efter {
+    //Hade svårt att hitta lösningar och de jag hittade var rätt svåra att sätta sig in i.
 }
