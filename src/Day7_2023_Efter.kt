436db0641897702cdad60e9c4ea4d76744f4eaf7
Aron Gyulai <arongyf@gmail.com>
2024-03-07 22:51:48 +0100

    Added comments

--- src/Day7_2023_Efter.kt
+++ src/Day7_2023_Efter.kt
@@ -1,5 +1,5 @@
 class Day7_2023_Efter {
-    //Har kollat på olika lösningar, de jag har hittat är snarlika min lösning.
-    //Eller också hade jag svårt att sätta mig in i dem. Companion Objects och Sequence är två
-    //av dem som jag ska försöka sätta mig in i.
+    //Har kollat på några få lösningar som jag kunde hitta. Tanken bakom var snarlika min lösning,
+    //men hade lite svårigheter att sätta mig in i dem. Companion Objects och Sequence är två
+    //nya ting som jag tänkte göra efterforskningar på.
 }
